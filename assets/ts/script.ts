32a9b4c28b6b1edd2c14aba4573d3e299841939e
CristianoAliotta <cristiano.aliotta@outlook.com>
2022-10-24 15:40:47 +0200

    logic comoleted

---
+++ assets/ts/script.ts
@@ -0,0 +1,21 @@
+let genitore1: number = 75;
+let genitore2: number = 13;
+let numberToGuess: number = Math.floor(Math.random() * 99 + 1)
+console.log(numberToGuess);
+
+function checker() {
+    if (genitore1 === numberToGuess || genitore2 === numberToGuess) {
+        if (genitore1 === numberToGuess) {
+            console.log("Complimenti genitore 1 hai indovinato!");
+        } else {
+            console.log("Complimenti genitore 2 hai indovinato!");
+        }
+    } else if ((Math.abs(numberToGuess - genitore1) < Math.abs(numberToGuess - genitore2))) {
+        console.log("Genitore 1 si è avvicinato di più di genitore 2");
+    } else {
+        console.log("Genitore 2 si è avvicinato di più di genitore 1");
+
+    }
+
+}
+checker()
